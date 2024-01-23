9e097bd1fbd3d1367048655fc6026be16e41c782
Jubbim0 <144752825+Jubbim0@users.noreply.github.com>
2024-01-24 01:04:43 +1100

    first commit!

---
+++ 8bit_even_odd.c
@@ -0,0 +1,521 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+int main(int argc, char* argv[])
+{
+    uint16_t number = atoi(argv[1]); // No problems here
+    if (number == 0) {
+        printf("even\n");
+    } else if (number == 1) {
+        printf("odd\n");
+    } else if (number == 2) {
+        printf("even\n");
+    } else if (number == 3) {
+        printf("odd\n");
+    } else if (number == 4) {
+        printf("even\n");
+    } else if (number == 5) {
+        printf("odd\n");
+    } else if (number == 6) {
+        printf("even\n");
+    } else if (number == 7) {
+        printf("odd\n");
+    } else if (number == 8) {
+        printf("even\n");
+    } else if (number == 9) {
+        printf("odd\n");
+    } else if (number == 10) {
+        printf("even\n");
+    } else if (number == 11) {
+        printf("odd\n");
+    } else if (number == 12) {
+        printf("even\n");
+    } else if (number == 13) {
+        printf("odd\n");
+    } else if (number == 14) {
+        printf("even\n");
+    } else if (number == 15) {
+        printf("odd\n");
+    } else if (number == 16) {
+        printf("even\n");
+    } else if (number == 17) {
+        printf("odd\n");
+    } else if (number == 18) {
+        printf("even\n");
+    } else if (number == 19) {
+        printf("odd\n");
+    } else if (number == 20) {
+        printf("even\n");
+    } else if (number == 21) {
+        printf("odd\n");
+    } else if (number == 22) {
+        printf("even\n");
+    } else if (number == 23) {
+        printf("odd\n");
+    } else if (number == 24) {
+        printf("even\n");
+    } else if (number == 25) {
+        printf("odd\n");
+    } else if (number == 26) {
+        printf("even\n");
+    } else if (number == 27) {
+        printf("odd\n");
+    } else if (number == 28) {
+        printf("even\n");
+    } else if (number == 29) {
+        printf("odd\n");
+    } else if (number == 30) {
+        printf("even\n");
+    } else if (number == 31) {
+        printf("odd\n");
+    } else if (number == 32) {
+        printf("even\n");
+    } else if (number == 33) {
+        printf("odd\n");
+    } else if (number == 34) {
+        printf("even\n");
+    } else if (number == 35) {
+        printf("odd\n");
+    } else if (number == 36) {
+        printf("even\n");
+    } else if (number == 37) {
+        printf("odd\n");
+    } else if (number == 38) {
+        printf("even\n");
+    } else if (number == 39) {
+        printf("odd\n");
+    } else if (number == 40) {
+        printf("even\n");
+    } else if (number == 41) {
+        printf("odd\n");
+    } else if (number == 42) {
+        printf("even\n");
+    } else if (number == 43) {
+        printf("odd\n");
+    } else if (number == 44) {
+        printf("even\n");
+    } else if (number == 45) {
+        printf("odd\n");
+    } else if (number == 46) {
+        printf("even\n");
+    } else if (number == 47) {
+        printf("odd\n");
+    } else if (number == 48) {
+        printf("even\n");
+    } else if (number == 49) {
+        printf("odd\n");
+    } else if (number == 50) {
+        printf("even\n");
+    } else if (number == 51) {
+        printf("odd\n");
+    } else if (number == 52) {
+        printf("even\n");
+    } else if (number == 53) {
+        printf("odd\n");
+    } else if (number == 54) {
+        printf("even\n");
+    } else if (number == 55) {
+        printf("odd\n");
+    } else if (number == 56) {
+        printf("even\n");
+    } else if (number == 57) {
+        printf("odd\n");
+    } else if (number == 58) {
+        printf("even\n");
+    } else if (number == 59) {
+        printf("odd\n");
+    } else if (number == 60) {
+        printf("even\n");
+    } else if (number == 61) {
+        printf("odd\n");
+    } else if (number == 62) {
+        printf("even\n");
+    } else if (number == 63) {
+        printf("odd\n");
+    } else if (number == 64) {
+        printf("even\n");
+    } else if (number == 65) {
+        printf("odd\n");
+    } else if (number == 66) {
+        printf("even\n");
+    } else if (number == 67) {
+        printf("odd\n");
+    } else if (number == 68) {
+        printf("even\n");
+    } else if (number == 69) {
+        printf("odd\n");
+    } else if (number == 70) {
+        printf("even\n");
+    } else if (number == 71) {
+        printf("odd\n");
+    } else if (number == 72) {
+        printf("even\n");
+    } else if (number == 73) {
+        printf("odd\n");
+    } else if (number == 74) {
+        printf("even\n");
+    } else if (number == 75) {
+        printf("odd\n");
+    } else if (number == 76) {
+        printf("even\n");
+    } else if (number == 77) {
+        printf("odd\n");
+    } else if (number == 78) {
+        printf("even\n");
+    } else if (number == 79) {
+        printf("odd\n");
+    } else if (number == 80) {
+        printf("even\n");
+    } else if (number == 81) {
+        printf("odd\n");
+    } else if (number == 82) {
+        printf("even\n");
+    } else if (number == 83) {
+        printf("odd\n");
+    } else if (number == 84) {
+        printf("even\n");
+    } else if (number == 85) {
+        printf("odd\n");
+    } else if (number == 86) {
+        printf("even\n");
+    } else if (number == 87) {
+        printf("odd\n");
+    } else if (number == 88) {
+        printf("even\n");
+    } else if (number == 89) {
+        printf("odd\n");
+    } else if (number == 90) {
+        printf("even\n");
+    } else if (number == 91) {
+        printf("odd\n");
+    } else if (number == 92) {
+        printf("even\n");
+    } else if (number == 93) {
+        printf("odd\n");
+    } else if (number == 94) {
+        printf("even\n");
+    } else if (number == 95) {
+        printf("odd\n");
+    } else if (number == 96) {
+        printf("even\n");
+    } else if (number == 97) {
+        printf("odd\n");
+    } else if (number == 98) {
+        printf("even\n");
+    } else if (number == 99) {
+        printf("odd\n");
+    } else if (number == 100) {
+        printf("even\n");
+    } else if (number == 101) {
+        printf("odd\n");
+    } else if (number == 102) {
+        printf("even\n");
+    } else if (number == 103) {
+        printf("odd\n");
+    } else if (number == 104) {
+        printf("even\n");
+    } else if (number == 105) {
+        printf("odd\n");
+    } else if (number == 106) {
+        printf("even\n");
+    } else if (number == 107) {
+        printf("odd\n");
+    } else if (number == 108) {
+        printf("even\n");
+    } else if (number == 109) {
+        printf("odd\n");
+    } else if (number == 110) {
+        printf("even\n");
+    } else if (number == 111) {
+        printf("odd\n");
+    } else if (number == 112) {
+        printf("even\n");
+    } else if (number == 113) {
+        printf("odd\n");
+    } else if (number == 114) {
+        printf("even\n");
+    } else if (number == 115) {
+        printf("odd\n");
+    } else if (number == 116) {
+        printf("even\n");
+    } else if (number == 117) {
+        printf("odd\n");
+    } else if (number == 118) {
+        printf("even\n");
+    } else if (number == 119) {
+        printf("odd\n");
+    } else if (number == 120) {
+        printf("even\n");
+    } else if (number == 121) {
+        printf("odd\n");
+    } else if (number == 122) {
+        printf("even\n");
+    } else if (number == 123) {
+        printf("odd\n");
+    } else if (number == 124) {
+        printf("even\n");
+    } else if (number == 125) {
+        printf("odd\n");
+    } else if (number == 126) {
+        printf("even\n");
+    } else if (number == 127) {
+        printf("odd\n");
+    } else if (number == 128) {
+        printf("even\n");
+    } else if (number == 129) {
+        printf("odd\n");
+    } else if (number == 130) {
+        printf("even\n");
+    } else if (number == 131) {
+        printf("odd\n");
+    } else if (number == 132) {
+        printf("even\n");
+    } else if (number == 133) {
+        printf("odd\n");
+    } else if (number == 134) {
+        printf("even\n");
+    } else if (number == 135) {
+        printf("odd\n");
+    } else if (number == 136) {
+        printf("even\n");
+    } else if (number == 137) {
+        printf("odd\n");
+    } else if (number == 138) {
+        printf("even\n");
+    } else if (number == 139) {
+        printf("odd\n");
+    } else if (number == 140) {
+        printf("even\n");
+    } else if (number == 141) {
+        printf("odd\n");
+    } else if (number == 142) {
+        printf("even\n");
+    } else if (number == 143) {
+        printf("odd\n");
+    } else if (number == 144) {
+        printf("even\n");
+    } else if (number == 145) {
+        printf("odd\n");
+    } else if (number == 146) {
+        printf("even\n");
+    } else if (number == 147) {
+        printf("odd\n");
+    } else if (number == 148) {
+        printf("even\n");
+    } else if (number == 149) {
+        printf("odd\n");
+    } else if (number == 150) {
+        printf("even\n");
+    } else if (number == 151) {
+        printf("odd\n");
+    } else if (number == 152) {
+        printf("even\n");
+    } else if (number == 153) {
+        printf("odd\n");
+    } else if (number == 154) {
+        printf("even\n");
+    } else if (number == 155) {
+        printf("odd\n");
+    } else if (number == 156) {
+        printf("even\n");
+    } else if (number == 157) {
+        printf("odd\n");
+    } else if (number == 158) {
+        printf("even\n");
+    } else if (number == 159) {
+        printf("odd\n");
+    } else if (number == 160) {
+        printf("even\n");
+    } else if (number == 161) {
+        printf("odd\n");
+    } else if (number == 162) {
+        printf("even\n");
+    } else if (number == 163) {
+        printf("odd\n");
+    } else if (number == 164) {
+        printf("even\n");
+    } else if (number == 165) {
+        printf("odd\n");
+    } else if (number == 166) {
+        printf("even\n");
+    } else if (number == 167) {
+        printf("odd\n");
+    } else if (number == 168) {
+        printf("even\n");
+    } else if (number == 169) {
+        printf("odd\n");
+    } else if (number == 170) {
+        printf("even\n");
+    } else if (number == 171) {
+        printf("odd\n");
+    } else if (number == 172) {
+        printf("even\n");
+    } else if (number == 173) {
+        printf("odd\n");
+    } else if (number == 174) {
+        printf("even\n");
+    } else if (number == 175) {
+        printf("odd\n");
+    } else if (number == 176) {
+        printf("even\n");
+    } else if (number == 177) {
+        printf("odd\n");
+    } else if (number == 178) {
+        printf("even\n");
+    } else if (number == 179) {
+        printf("odd\n");
+    } else if (number == 180) {
+        printf("even\n");
+    } else if (number == 181) {
+        printf("odd\n");
+    } else if (number == 182) {
+        printf("even\n");
+    } else if (number == 183) {
+        printf("odd\n");
+    } else if (number == 184) {
+        printf("even\n");
+    } else if (number == 185) {
+        printf("odd\n");
+    } else if (number == 186) {
+        printf("even\n");
+    } else if (number == 187) {
+        printf("odd\n");
+    } else if (number == 188) {
+        printf("even\n");
+    } else if (number == 189) {
+        printf("odd\n");
+    } else if (number == 190) {
+        printf("even\n");
+    } else if (number == 191) {
+        printf("odd\n");
+    } else if (number == 192) {
+        printf("even\n");
+    } else if (number == 193) {
+        printf("odd\n");
+    } else if (number == 194) {
+        printf("even\n");
+    } else if (number == 195) {
+        printf("odd\n");
+    } else if (number == 196) {
+        printf("even\n");
+    } else if (number == 197) {
+        printf("odd\n");
+    } else if (number == 198) {
+        printf("even\n");
+    } else if (number == 199) {
+        printf("odd\n");
+    } else if (number == 200) {
+        printf("even\n");
+    } else if (number == 201) {
+        printf("odd\n");
+    } else if (number == 202) {
+        printf("even\n");
+    } else if (number == 203) {
+        printf("odd\n");
+    } else if (number == 204) {
+        printf("even\n");
+    } else if (number == 205) {
+        printf("odd\n");
+    } else if (number == 206) {
+        printf("even\n");
+    } else if (number == 207) {
+        printf("odd\n");
+    } else if (number == 208) {
+        printf("even\n");
+    } else if (number == 209) {
+        printf("odd\n");
+    } else if (number == 210) {
+        printf("even\n");
+    } else if (number == 211) {
+        printf("odd\n");
+    } else if (number == 212) {
+        printf("even\n");
+    } else if (number == 213) {
+        printf("odd\n");
+    } else if (number == 214) {
+        printf("even\n");
+    } else if (number == 215) {
+        printf("odd\n");
+    } else if (number == 216) {
+        printf("even\n");
+    } else if (number == 217) {
+        printf("odd\n");
+    } else if (number == 218) {
+        printf("even\n");
+    } else if (number == 219) {
+        printf("odd\n");
+    } else if (number == 220) {
+        printf("even\n");
+    } else if (number == 221) {
+        printf("odd\n");
+    } else if (number == 222) {
+        printf("even\n");
+    } else if (number == 223) {
+        printf("odd\n");
+    } else if (number == 224) {
+        printf("even\n");
+    } else if (number == 225) {
+        printf("odd\n");
+    } else if (number == 226) {
+        printf("even\n");
+    } else if (number == 227) {
+        printf("odd\n");
+    } else if (number == 228) {
+        printf("even\n");
+    } else if (number == 229) {
+        printf("odd\n");
+    } else if (number == 230) {
+        printf("even\n");
+    } else if (number == 231) {
+        printf("odd\n");
+    } else if (number == 232) {
+        printf("even\n");
+    } else if (number == 233) {
+        printf("odd\n");
+    } else if (number == 234) {
+        printf("even\n");
+    } else if (number == 235) {
+        printf("odd\n");
+    } else if (number == 236) {
+        printf("even\n");
+    } else if (number == 237) {
+        printf("odd\n");
+    } else if (number == 238) {
+        printf("even\n");
+    } else if (number == 239) {
+        printf("odd\n");
+    } else if (number == 240) {
+        printf("even\n");
+    } else if (number == 241) {
+        printf("odd\n");
+    } else if (number == 242) {
+        printf("even\n");
+    } else if (number == 243) {
+        printf("odd\n");
+    } else if (number == 244) {
+        printf("even\n");
+    } else if (number == 245) {
+        printf("odd\n");
+    } else if (number == 246) {
+        printf("even\n");
+    } else if (number == 247) {
+        printf("odd\n");
+    } else if (number == 248) {
+        printf("even\n");
+    } else if (number == 249) {
+        printf("odd\n");
+    } else if (number == 250) {
+        printf("even\n");
+    } else if (number == 251) {
+        printf("odd\n");
+    } else if (number == 252) {
+        printf("even\n");
+    } else if (number == 253) {
+        printf("odd\n");
+    } else if (number == 254) {
+        printf("even\n");
+    } else if (number == 255) {
+        printf("odd\n");
+    }
+    return 0;
+}
